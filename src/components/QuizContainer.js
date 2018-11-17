f8ff2f8f25c1db710ea05aec7c421acd2c5d9144
Andrew Golightly <support@andrewgolightly.com>
2018-11-17 13:15:50 -0700

    added Firebase functionality

--- src/components/QuizContainer.js
+++ src/components/QuizContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import AppBar from './AppBar';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
@@ -7,6 +8,25 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 const styles = theme => ({});
 
 class QuizContainer extends Component {
+  state = {};
+  componentDidMount = async () => {
+    // Initialize Firebase
+    const config = {
+      apiKey: 'AIzaSyAz8vbYYqBYwqRo_Me4gzSaLAUQ3Dj--4c',
+      authDomain: 'sassy-sensei.firebaseapp.com',
+      databaseURL: 'https://sassy-sensei.firebaseio.com',
+      projectId: 'sassy-sensei',
+      storageBucket: 'sassy-sensei.appspot.com',
+      messagingSenderId: '517378806197'
+    };
+    firebase.initializeApp(config);
+    const snapshot = await firebase
+      .database()
+      .ref('/')
+      .once('value');
+    this.setState(snapshot.val());
+  };
+
   render() {
     return (
       <React.Fragment>
